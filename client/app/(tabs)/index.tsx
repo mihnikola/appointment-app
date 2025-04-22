e33166bd8fbc90c0e7fb84a468757dc9fd461f1d
mihnikola <mihailovic93.nikola@gmail.com>
2025-04-22 14:23:44 +0200

    test ne valja

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Text,
   View,
@@ -7,34 +7,15 @@ import {
   Image,
   TouchableHighlight,
 } from "react-native";
-import * as Notifications from "expo-notifications";
-import {
-  getExpoPushTokenAsync,
-  requestPermissionsAsync,
-} from "expo-notifications";
+
 import { useNavigation } from "@react-navigation/native";
 import { createOpenLink } from "react-native-open-maps";
 import FlatButton from "@/shared-components/Button";
 import AboutUsInfo from "@/app/components/home/AboutUsInfo";
 import ListAboutUs from "@/app/components/home/ListAboutUs";
 import OnboardingComponent from "@/components/OnboardingComponent";
-import { getStorage } from "@/helpers";
-import axios from "axios";
 import { MAIN_DATA } from "@/constants";
 
-// Handle background notifications using Expo's background handler
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
-
-Notifications.addNotificationReceivedListener((notification) => {
-  console.log("Background notification received:", notification);
-  // Handle the background notification
-});
 
 const yosemite = { latitude: 43.724943, longitude: 20.6952 };
 
@@ -46,61 +27,8 @@ export default function HomeScreen() {
   const nextPage = () => {
     navigation.navigate("(tabs)", { screen: "employers" });
   };
-  const [notification, setNotification] = useState(false);
 
-  const getPushToken = async () => {
-    // Request permission for notifications
-    const { status } = await requestPermissionsAsync();
-    if (status === "granted") {
-      // Get the Expo Push Token (which is equivalent to FCM Token in this case)
-      const token = await getExpoPushTokenAsync();
-      // Optionally, send this token to your backend (Node.js)
-      sendTokenToServer(token.data);
-    }
-  };
-  useEffect(() => {
-    getPushToken();
-    const notificationListener = Notifications.addNotificationReceivedListener(
-      (notification) => {
-        console.log("addNotificationReceivedListener++++", notification);
-        setNotification(notification);
-      }
-    );
-
-    const responseListener =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        // console.log("addNotificationResponseReceivedListener++++",response.notification.request.content.body);
-        setNotification(response);
-        // navigation.navigate("components/services/menuservices");
-      });
-
-    // Clean up the listeners when the component is unmounted
-
-    return () => {
-      notificationListener.remove();
-      responseListener.remove();
-    };
-  }, []);
-
-  // Send the push token to your server (Node.js backend) ok
-  const sendTokenToServer = async (data) => {
-    try {
-      await axios
-        .post(`${process.env.EXPO_PUBLIC_API_URL}/api/save-token`, {
-          tokenExpo: data,
-          tokenUser:null,
-        })
-        .then((res) => {
-          console.log(res);
-          if (res.status === 404) {
-            console.log("Prc vec ima");
-          }
-        });
-    } catch (error) {
-      console.error("Error sending token to server:", error);
-    }
-  };
-
+ 
 
   return (
     <ScrollView style={styles.container}>
--- client/app/components/reservation/index.tsx
+++ client/app/components/reservation/index.tsx
@@ -1,16 +1,101 @@
 import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReservationContext from "@/context/ReservationContext";
 import ButtonComponent from "@/shared-components/Button";
 import Details from "@/shared-components/Details";
 import Note from "@/shared-components/Note";
-import { addMinutesToTime, convertDate } from "@/helpers";
+import { addMinutesToTime, convertDate, getStorage } from "@/helpers";
 import useSubmitReservation from "./hooks/useSubmitReservation";
+import axios from "axios";
+import * as Notifications from "expo-notifications";
+import {
+  getExpoPushTokenAsync,
+  requestPermissionsAsync,
+} from "expo-notifications";
+
+
+// Handle background notifications using Expo's background handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+Notifications.addNotificationReceivedListener((notification) => {
+  console.log("Background notification received:", notification);
+  // Handle the background notification
+});
+
 
 const Reservation = () => {
   const { reservation } = useContext(ReservationContext)!;
   const { employer, service, timeData, dateReservation } = reservation;
   const { submitReservationHandler, isLoading, error } = useSubmitReservation();
+
+  const [notification, setNotification] = useState(false);
+
+  const getPushToken = async (tokenUser) => {
+    // Request permission for notifications
+    const { status } = await requestPermissionsAsync();
+    if (status === "granted") {
+      // Get the Expo Push Token (which is equivalent to FCM Token in this case)
+      const token = await getExpoPushTokenAsync();
+      // Optionally, send this token to your backend (Node.js)
+      sendTokenToServer(token.data,tokenUser);
+    }
+  };
+  useEffect(() => {
+    getStorage().then((res) => {
+      if(res){
+        getPushToken(res);
+      }
+    })
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("addNotificationReceivedListener++++", notification);
+        setNotification(notification);
+      }
+    );
+
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        // console.log("addNotificationResponseReceivedListener++++",response.notification.request.content.body);
+        setNotification(response);
+        // navigation.navigate("components/services/menuservices");
+      });
+
+    // Clean up the listeners when the component is unmounted
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
+
+
+
+
+   // Send the push token to your server (Node.js backend) ok
+   const sendTokenToServer = async (data,userToken) => {
+    try {
+      await axios
+        .post(`${process.env.EXPO_PUBLIC_API_URL}/api/save-token`, {
+          tokenExpo: data,
+          tokenUser:userToken,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.status === 404) {
+            console.log("Prc vec ima");
+          }
+        });
+    } catch (error) {
+      console.error("Error sending token to server:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image
